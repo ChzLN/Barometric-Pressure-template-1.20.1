0ab5c061b5edc14076307842ae728a8c58c62a7d
TaoYao <ys2911496979@163.com>
2025-10-25 06:33:31 +0800

    TaoYao

---
+++ src/main/java/com/atmosphere/BarometricPressureClient.java
@@ -0,0 +1,10 @@
+package com.atmosphere;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BarometricPressureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/atmosphere/BarometricPressureDataGenerator.java
@@ -0,0 +1,11 @@
+package com.atmosphere;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BarometricPressureDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
